9b4f46d8c6c5226c86da83c77aa1e17623b611a2
Henry\Cito <penroll55@yahoo.com>
2024-03-10 19:34:52 -0500

    created filter response api

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express')
+const axios = require('axios');
+
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send('Hello Node API')
-})
+
+app.get('/:formId/filteredResponses', (req, res) => {
+    const formId = req.params.formId;
+    const filterOptions = req.query.filters;
+
+    try{
+        const filterData = JSON.parse(filterOptions);
+        if(!filterData){
+            return res.status(400).json({ error: 'Missing filter parameters' }); 
+        }
+
+        axios.get(`https://api.fillout.com/v1/api/forms/${formId}`, {
+            headers: {
+                'Authorization': `Bearer sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912`
+            },
+            params: {
+                filters: filterData
+            }
+        })
+        .then((response)=>{
+            res.status(200).json(response.data);
+        })
+        .catch((error)=>{
+            res.status(500).json({ error: 'An Internal Server Error Occurs' });
+        });
+    }
+    catch(error){
+        res.status(500).json({error: error.message});
+    }
+});
 
 app.listen(3000, ()=>{
-    console.log('NOde API is runing on port 3000')
+    console.log('Node API is runing on port http://localhost:3000');
 })
